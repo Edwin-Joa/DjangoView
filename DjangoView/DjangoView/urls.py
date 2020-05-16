048a7c7df51afe9f619b8fafbf120c5ad27f8f7f
EdwinZ <132456@qq.com>
2020-05-16 16:46:09 +0800

    修改之前写的错误代码

--- DjangoView/DjangoView/urls.py
+++ DjangoView/DjangoView/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('users/register',include('users.urls'))
+    path('',include('users.urls'))
 ]
