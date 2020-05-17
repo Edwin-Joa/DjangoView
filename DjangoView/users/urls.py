6f22bcf67711b20e2556976ce61d39a36cb5cb75
EdwinZ <132456@qq.com>
2020-05-17 11:29:04 +0800

    添加了扩展类，测试了re_path与url的功能

--- DjangoView/users/urls.py
+++ DjangoView/users/urls.py
@@ -1,9 +1,12 @@
-from django.urls import path
+from django.urls import path,re_path
+from django.conf.urls import url
 from . import views
 
 
 urlpatterns = [
-	path('users/register/',views.RegisterView.as_view()),
+	# path('users/register/',views.RegisterView.as_view()),
+	re_path(r'^users/register/$',views.RegisterView.as_view()),
+	url(r'^users/login/$',views.LoginView.as_view()),
 ]
 
 
--- DjangoView/users/views.py
+++ DjangoView/users/views.py
@@ -3,17 +3,29 @@ from django import http
 from django.views import View
 # Create your views here.
 
-class RegisterView(View):
+class RegisterMixin(object):
+    def pri(self):
+        print('这是一个拓展功能类的功能')
+
+class RegisterView(View,RegisterMixin):
     def get(self,request):
+        self.pri()
         return http.HttpResponse('GET请求得到的画面')
 
     def post(self,request):
         return http.HttpResponse('POST请求得到的画面')
 
+class LoginView(View):
+    def get(self,request):
+        return http.HttpResponse('GET得到的登录界面')
+
+    def post(self,request):
+        return http.HttpResponse('POST得到的登录界面')
+
+
 
 # def register(request):
 # 	return http.HttpResponse('这是一个注册页面')
-
 
 
 
